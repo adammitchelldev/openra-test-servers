74d1387f4a5f1bc1591724444b856d76c703cb4b
Adam Mitchell <adammitchelldev@gmail.com>
2021-01-17 19:29:13 +0000

    Update version

--- download-appimage.sh
+++ download-appimage.sh
@@ -2,7 +2,7 @@
 set -ex
 
 REPO=${REPO:-"adammitchelldev"}
-VERSION=${VERSION:-"devtest-netcode-2"}
+VERSION=${VERSION:-"devtest-netcode-3"}
 IMAGE=${IMAGE:-"OpenRA-Red-Alert-devel-x86_64.AppImage"}
 
 curl -L -O "https://github.com/${REPO}/OpenRA/releases/download/${VERSION}/${IMAGE}"
